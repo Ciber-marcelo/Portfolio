e7429efbf2654ef3217264d6454386f762857962
Ciber-marcelo <marcelones125@gmail.com>
2023-11-20 21:07:35 -0300

    chore: footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,17 +2,15 @@ import Image from "next/image";
 
 export function Footer() {
    return (
-      <div className="flex justify-center bg-gray-950 pb-6 pt-16">
-         <div className="flex items-center gap-3 font-mono text-gray-400">
-            <Image
-               className='bg-color1bg rounded-lg p-1'
-               width={30}
-               height={30}
-               src='/images/M.svg'
-               alt='Logo Marcelo'
-            />
-            Desenvolvido por Marcelones
-         </div>
+      <div className="flex justify-center items-center gap-2 bg-gray-950 pb-8 pt-16 font-mono text-gray-400">
+         <Image
+            className='w-12 h-12 bg-color1bg rounded-md p-1'
+            width={30}
+            height={30}
+            src='/images/M.svg'
+            alt='Logo Marcelo'
+         />
+         Desenvolvido por Marcelones
       </div>
    )
 }
